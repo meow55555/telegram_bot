2e203ce46ab5649a0d47d41ebf440fc4ec9a34d5
Ma Sean <85484530+seanmamasde@users.noreply.github.com>
2021-10-24 11:24:20 +0800

    test 4

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def find_at(msg):
             return i
 
 
-@bot.message_handler(commands=['status'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, 'Welcome!')
 
